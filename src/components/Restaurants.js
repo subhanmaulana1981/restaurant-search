8d6730bc20cbbf0a35871159516294847e08afd6
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2022-09-10 13:10:48 +0700

    testing jsonplaceholder

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,25 +1,21 @@
-import { 
-    View, 
-    StyleSheet,
-    Text 
-} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import yelp from "../api/yelp";
 
 export default function Restaurants() {
 
-    const searchTodos = async () => {
-        const response = await yelp.get("/todos", {
-            params: {
-                limit: 1,
-            }
-        });
+  const searchTodos = async () => {
+    const response = await yelp.get("/todos/1", {
+      params: {
+        limit: 1
+      },
+    });
 
-        console.log(response);
-    }
+    console.log(response);
+  };
 
-    searchTodos();
+  searchTodos();
 
-    /* const searchRestaurants = async () => {
+  /* const searchRestaurants = async () => {
         const response = await yelp.get("/search", {
             params: {
                 limit: 1,
@@ -33,26 +29,24 @@ export default function Restaurants() {
 
     searchRestaurants(); */
 
-    return (
-        <View style={ styles.container }>
-            <Text style={ styles.header }>
-                Top Restaurants
-            </Text>            
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Top Restaurants</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 25,
-        marginVertical: 15,
-        flex: 1,
-    },
+  container: {
+    marginHorizontal: 25,
+    marginVertical: 15,
+    flex: 1,
+  },
 
-    header: {
-        fontWeight: "bold",
-        fontSize: 20,
-        paddingBottom: 10,
-        height: 100,
-    },
+  header: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 10,
+    height: 100,
+  },
 });
